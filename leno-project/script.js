6b998103d11ecc4dc066314153a33da44c119ecf
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-04-23 17:58:53 +0600

    updated

--- leno-project/script.js
+++ leno-project/script.js
@@ -1,26 +1,24 @@
-const toggleBtn = document.querySelector(".navbar__mobile-menu-toggle");
-const mobileMenu = document.querySelector(".navbar__mobile-menu-items");
-const navbar = document.querySelector(".navbar");
-const dialog = document.querySelector("dialog");
-// console.log(toggleBtn);
-// console.log(mobileMenu);
-// console.log(navbar);
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.querySelector(".navbar__mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar__mobile-menu-items");
+  const navbar = document.querySelector(".navbar");
+  const dialog = document.querySelector("dialog");
 
-toggleBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("show");
-});
+  toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show");
+  });
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    navbar.classList.add("navbar-scroll");
-  } else {
-    navbar.classList.remove("navbar-scroll");
-  }
-});
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add("navbar-scroll");
+    } else {
+      navbar.classList.remove("navbar-scroll");
+    }
+  });
 
-dialog.addEventListener("click", (e) => {
-  if (e.target === dialog) {
-    dialog.close();
-    stopVideo();
-  }
+  dialog.addEventListener("click", (e) => {
+    if (e.target === dialog) {
+      dialog.close();
+    }
+  });
 });
