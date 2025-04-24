843050de64e33d28d7d923a314fe01d2cf29b1c8
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-04-24 20:29:00 +0600

    updated

--- tutor-website/script.js
+++ tutor-website/script.js
@@ -1,17 +1,17 @@
-const toggleBtn = document.querySelector(".mobile-menu-toogle");
-const mobileMenu = document.querySelector(".mobile-menu");
-const navbar = document.querySelector(".navbar");
-// console.log(toggleBtn);
-// console.log(mobileMenu);
-toggleBtn.addEventListener("click", () => {
-  //   toggleBtn.classList.toggle("open");
-  mobileMenu.classList.toggle("activate");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.querySelector(".mobile-menu-toogle");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    navbar.classList.add("navbar-scroll");
-  } else {
-    navbar.classList.remove("navbar-scroll");
-  }
+  toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("activate");
+  });
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add("navbar-scroll");
+    } else {
+      navbar.classList.remove("navbar-scroll");
+    }
+  });
 });
