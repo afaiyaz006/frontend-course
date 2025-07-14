a4a4f10184c612eeaae69609c3f01612a7a58cee
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-07-14 19:48:24 +0600

    javascript task added

---
+++ javascript-advanced-task/quote-generator/script.js
@@ -0,0 +1,35 @@
+var datas = [];
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+  getQuotes();
+});
+function getRandomNumber() {
+  let randomString = Math.random() * 10;
+  let randomNumber = +randomString.toString()[0];
+  return randomNumber % 10;
+}
+async function getQuotes() {
+  const url = "https://6874f813dd06792b9c9618b9.mockapi.io/Quotes";
+
+  if (datas.length == 0) {
+    document.getElementById("quote-card").style.display = "none";
+    document.getElementById("loader").style.display = "flex";
+    const response = await fetch(url);
+    if (response.ok) {
+      const jsonData = await response.json();
+      datas = jsonData;
+
+      let randomQuote = jsonData[getRandomNumber()];
+      console.log(randomQuote);
+      document.getElementById("quote-data").innerHTML = randomQuote.quote;
+      document.getElementById("author-data").innerHTML = randomQuote.author;
+      document.getElementById("quote-card").style.display = "flex";
+      document.getElementById("loader").style.display = "none";
+    }
+  }
+}
+function onNewQuoteButtonClicked() {
+  randomQuote = datas[getRandomNumber()];
+  document.getElementById("quote-data").innerHTML = randomQuote.quote;
+  document.getElementById("author-data").innerHTML = randomQuote.author;
+}
