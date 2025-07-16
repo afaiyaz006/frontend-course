bd14ede3d7a61cd19441874cee0701cf0ad3eeac
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-07-16 19:19:26 +0600

    added project infinite scroll

---
+++ javascript-advanced-task/infinity-scroll/script.js
@@ -0,0 +1,44 @@
+var datas = [];
+var page = 0;
+function addImages(imageUrls) {
+  let data = "";
+  imageUrls.forEach((url) => (data += `<img src=${url}/>`));
+//   console.log(data);
+//   console.log(document.getElementById("images"));
+  document.getElementById("images").innerHTML += data;
+}
+function showLoadingSpinner() {
+  document.getElementById("spinner").style.display = "inline-block";
+}
+function hideLoadingSpinner() {
+  document.getElementById("spinner").style.display = "none";
+}
+async function getImages(page, limit = 10) {
+  const url = `https://picsum.photos/v2/list?page=${page}&limit=${limit}`;
+  showLoadingSpinner();
+  const response = await fetch(url);
+
+//   console.log(response);
+  if (response.ok) {
+
+    const jsonData = await response.json();
+    datas = jsonData;
+    console.log("API RESPONSE ")
+    console.log(jsonData)
+    imageUrls = [];
+    datas.forEach((item) => imageUrls.push(item.download_url));
+    addImages(imageUrls);
+    hideLoadingSpinner();
+  }
+}
+window.addEventListener("scroll", function () {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    console.log("Scrolled to the bottom of the page!");
+    getImages(page + 1);
+    console.log(page);
+    page += 1;
+  }
+});
+document.addEventListener("DOMContentLoaded", (event) => {
+  getImages(page);
+});
